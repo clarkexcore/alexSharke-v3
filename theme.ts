915808ed55bb6dbb07c2b93fdd970788fa01d80b
Alex Clarke <clarkexcore@gmail.com>
2024-08-11 20:45:52 -0400

    feat: Add Google Fonts to improve typography and styling
    This commit adds Google Fonts to the project, specifically the "Fira Sans" and "Montserrat" font families. The fonts are loaded using a link tag in the HTML head section of the layout component. This improves the typography and overall styling of the application.
    
    Code changes:
    - Added import statement for the "Head" component from the "next/head" package in the layout component
    - Added link tags for preconnecting to the Google Fonts API and loading the "Fira Sans" and "Montserrat" font families in the HTML head section of the layout component
    - Updated the theme configuration in the "theme.ts" file to use the "Montserrat" font family for the entire application, and set specific font weights and sizes for different typography elements
    
    Recent user commits:
    - chore: Add Supabase client and related changes
    - chore: Add header component to home page
    - Cleaning up/adding mui
    - Initial commit from Create Next App
    
    Recent repository commits:
    - chore: Add Supabase client and related changes
    - chore: Add header component to home page
    - Cleaning up/adding mui
    - Initial commit from Create Next App

--- theme.ts
+++ theme.ts
@@ -1,7 +1,33 @@
 "use client";
 
+// import Montserrant from "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap";
 import { createTheme } from "@mui/material/styles";
 
-const theme = createTheme({});
+const theme = createTheme({
+	typography: {
+        fontFamily: "Montserrat, sans-serif",
+        h1: {
+            fontWeight: 900
+        },
+        body1: {
+            fontFamily: "Fira Sans, sans-serif",
+            fontWeight: 300,
+            fontSize: 14,
+        },
+        body2: {
+            fontFamily: "Fira Sans, sans-serif",
+            fontWeight: 200,
+            fontSize: 12
+        }
+    },
+    palette: {
+        primary: {
+            main: "#000"
+        },
+        secondary: {
+            main: "#fff",
+        }
+    }
+});
 
 export default theme;
