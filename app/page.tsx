aa587a764f0b76240f62c6109ca1bf1987a0e336
clarkexcore <clarkexcore@gmail.com>
2024-08-14 17:47:10 -0400

    testing stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
 import Header from "@/components/home/header.component";
+import { supabase } from "@/lib/supabaseClient";
 import { Box } from "@mui/material";
 
 export default function Home() {
+	const getAdminUsersTable = async () => {
+		try {
+			const { data, error } = await supabase
+				.from("admin_users")
+				.select("*")
+				.select();
+
+			if (error !== null) {
+				throw Error("Error fetching admin users table");
+			}
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	getAdminUsersTable();
 	return (
 		<Box>
 			<Header />
