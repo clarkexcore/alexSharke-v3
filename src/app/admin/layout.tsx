b55153691e7726ad317e2698eb43bbe2a756dec6
clarkexcore <clarkexcore@gmail.com>
2024-12-17 16:31:21 -0500

    workin 4 da weekend

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import { Box } from "@mui/material";
+import { useRef, useState } from "react";
+
 export default function AdminLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
-	return <>{children}</>;
+	const [mobileMenuOpen, setMobileMenueOpen] = useState<boolean>(false);
+	const containerRef = useRef<HTMLElement>(null);
+
+	return (
+		<>
+			<Box sx={{ backgroundColor: "#f2f2f2", minHeight: "100vh" }}>
+				{children}
+			</Box>
+		</>
+	);
 }
